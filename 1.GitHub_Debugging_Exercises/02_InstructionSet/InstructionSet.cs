5a04d74933bc0f56cfa81b340db93b7de702ef42
Deyan <dublinstand@gmail.com>
2016-12-13 13:27:46 +0000

    new file:   01_HolidaysBetweenTwoDates/01_HolidaysBetweenTwoDates.csproj 	new file:   01_HolidaysBetweenTwoDates/App.config 	new file:   01_HolidaysBetweenTwoDates/HolidaysBetweenTwoDates.cs 	new file:   01_HolidaysBetweenTwoDates/Properties/AssemblyInfo.cs 	new file:   02_InstructionSet/02_InstructionSet.csproj 	new file:   02_InstructionSet/App.config 	new file:   02_InstructionSet/InstructionSet.cs 	new file:   02_InstructionSet/Properties/AssemblyInfo.cs 	new file:   03_BePositive/03_BePositive.csproj 	new file:   03_BePositive/App.config 	new file:   03_BePositive/BePositive.cs 	new file:   03_BePositive/Properties/AssemblyInfo.cs 	new file:   04_SequenceOfCommands/04_SequenceOfCommands.csproj 	new file:   04_SequenceOfCommands/App.config 	new file:   04_SequenceOfCommands/Properties/AssemblyInfo.cs 	new file:   04_SequenceOfCommands/SequenceOfCommands.cs 	new file:   05_Substring/05_Substring.csproj 	new file:   05_Substring/App.config 	new file:   05_Substring/Properties/AssemblyInfo.cs 	new file:   05_Substring/Substring.cs 	new file:   1.GitHub_Debugging_Exercises.sln

---
+++ 1.GitHub_Debugging_Exercises/02_InstructionSet/InstructionSet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class InstructionSet
+{
+    static void Main()
+    {
+
+        string opCode = "";
+
+        while (opCode != "END")
+        {
+            opCode = Console.ReadLine();
+            string[] codeArgs = opCode.Split(' ');
+
+            long result = 0;
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = ++operandOne;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = --operandOne;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result =  operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+                case "END":
+                    {
+
+                        return;
+                    }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 1.GitHub_Debugging_Exercises/03_BePositive/BePositive.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BePositive
+{
+    public static void Main()
+    {
+        int countSequences = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countSequences; i++)
+        {
+            string[] input = Console.ReadLine().Trim().Split(' ');      //Trim clears the tabs in the end of the array, Split(' ') - splits the records by space
+            List<int> numbers = new List<int>();
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (!input[j].Equals(string.Empty))
+                {
+                    int num = int.Parse(input[j]);
+                    numbers.Add(num);
+                }
+            }
+
+            bool found = false;
+
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                int currentNum = numbers[j];
+
+                if (currentNum >= 0)
+                {
+                    Console.Write(currentNum);
+                    found = true;
+
+                    if (found)
+                    {
+                        Console.Write(" ");
+                    }                    
+                }
+                else if (j + 1 < numbers.Count)
+                {
+                    currentNum += numbers[j + 1];
+                    j++;
+
+                    if (currentNum >= 0)
+                    {
+                        Console.Write(currentNum);
+                        found = true;
+
+                        if (found)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("(empty)");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
