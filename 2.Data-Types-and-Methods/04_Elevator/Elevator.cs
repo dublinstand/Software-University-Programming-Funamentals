c2a141fa72cf4b0f0bd453c6c2b9ff52b4b25ac6
Deyan <dublinstand@gmail.com>
2016-12-14 10:02:10 +0000

    new file:   2.Data-Types-and-Methods/04_Elevator/04_Elevator.csproj 	new file:   2.Data-Types-and-Methods/04_Elevator/App.config 	new file:   2.Data-Types-and-Methods/04_Elevator/Elevator.cs 	new file:   2.Data-Types-and-Methods/04_Elevator/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/05_SpecialNumbers/05_SpecialNumbers.csproj 	new file:   2.Data-Types-and-Methods/05_SpecialNumbers/App.config 	new file:   2.Data-Types-and-Methods/05_SpecialNumbers/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/05_SpecialNumbers/SpecialNumbers.cs 	new file:   2.Data-Types-and-Methods/06_TripleOfLetters/06_TripleOfLetters.csproj 	new file:   2.Data-Types-and-Methods/06_TripleOfLetters/App.config 	new file:   2.Data-Types-and-Methods/06_TripleOfLetters/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/06_TripleOfLetters/TripleOfLetters.cs 	modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln

---
+++ 2.Data-Types-and-Methods/04_Elevator/Elevator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Elevator
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int courses = n / p;
+        if (n % p != 0)
+        {
+            courses++;
+        }
+
+        //int courses2 = (int)Math.Ceiling((double) n / p);  - Math.Celeing rounds to the higher number
+
+        Console.WriteLine(courses);
+    }
+}
+
---
+++ 2.Data-Types-and-Methods/05_SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class SpecialNumbers
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            int num2 = i;
+            int sum = 0;
+
+            while (num2 != 0)
+            {
+                sum += num2 % 10;
+                num2 = num2 / 10;
+
+            }
+
+            bool special = (sum == 5 || sum == 7 || sum == 11);
+            Console.WriteLine("{0} -> {1}", i, special);
+
+            //if (sum == 5 || sum == 7 || sum == 11)
+            //{
+            //    Console.WriteLine("{0} -> True", i);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} -> False", i);
+            //}
+        }
+    }
+}
+
