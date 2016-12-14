05d3977d07eaff63d2737df0424d188aaa9e1b62
Deyan <dublinstand@gmail.com>
2016-12-14 13:56:13 +0000

    new file:   2.Data-Types-and-Methods/07_Greeting/07_Greeting.csproj 	new file:   2.Data-Types-and-Methods/07_Greeting/App.config 	new file:   2.Data-Types-and-Methods/07_Greeting/Greeting.cs 	new file:   2.Data-Types-and-Methods/07_Greeting/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/08_DayOfWeek/08_DayOfWeek.csproj 	new file:   2.Data-Types-and-Methods/08_DayOfWeek/App.config 	new file:   2.Data-Types-and-Methods/08_DayOfWeek/DayOfWeek.cs 	new file:   2.Data-Types-and-Methods/08_DayOfWeek/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/09_FilledSquare/09_FilledSquare.csproj 	new file:   2.Data-Types-and-Methods/09_FilledSquare/App.config 	new file:   2.Data-Types-and-Methods/09_FilledSquare/FilledSquare.cs 	new file:   2.Data-Types-and-Methods/09_FilledSquare/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/10_PracticeIntegers/10_PracticeIntegers.csproj 	new file:   2.Data-Types-and-Methods/10_PracticeIntegers/App.config 	new file:   2.Data-Types-and-Methods/10_PracticeIntegers/PracticeIntegers.cs 	new file:   2.Data-Types-and-Methods/10_PracticeIntegers/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/11_PracticeFloatingPointNumbers/11_PracticeFloatingPointNumbers.csproj 	new file:   2.Data-Types-and-Methods/11_PracticeFloatingPointNumbers/App.config 	new file:   2.Data-Types-and-Methods/11_PracticeFloatingPointNumbers/PracticeFloatingPointNumbers.cs 	new file:   2.Data-Types-and-Methods/11_PracticeFloatingPointNumbers/Properties/AssemblyInfo.cs 	modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln

---
+++ 2.Data-Types-and-Methods/09_FilledSquare/FilledSquare.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class FilledSquare
+{
+    
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        printHeaderRow(n);
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            printMiddleRows(n);
+        }
+
+        printHeaderRow(n);
+    }
+
+    static void printHeaderRow(int n)
+    {
+        Console.WriteLine(new string('-', 2 * n));
+    }
+
+    static void printMiddleRows(int n)
+    {
+        Console.Write("-");
+        for (int i = 0; i < n - 1; i++)
+        {
+            Console.Write(@"\/");
+        }
+        Console.WriteLine("-");
+    }
+}
+
