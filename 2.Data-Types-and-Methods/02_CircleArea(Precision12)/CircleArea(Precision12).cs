a514689158f4e0619c046c2069aa6d184e22897d
Deyan <dublinstand@gmail.com>
2016-12-14 09:01:28 +0000

    modified:   2.Data-Types-and-Methods/01_CenturiesToMinutes/01_CenturiesToMinutes.csproj 	modified:   2.Data-Types-and-Methods/01_CenturiesToMinutes/CenturiesToMinutes.cs 	new file:   2.Data-Types-and-Methods/02_CircleArea(Precision12)/02_CircleArea(Precision12).csproj 	new file:   2.Data-Types-and-Methods/02_CircleArea(Precision12)/App.config 	new file:   2.Data-Types-and-Methods/02_CircleArea(Precision12)/CircleArea(Precision12).cs 	new file:   2.Data-Types-and-Methods/02_CircleArea(Precision12)/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/03_ExactSumOfRealNumbers/03_ExactSumOfRealNumbers.csproj 	new file:   2.Data-Types-and-Methods/03_ExactSumOfRealNumbers/App.config 	new file:   2.Data-Types-and-Methods/03_ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs 	new file:   2.Data-Types-and-Methods/03_ExactSumOfRealNumbers/Properties/AssemblyInfo.cs 	modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln

---
+++ 2.Data-Types-and-Methods/02_CircleArea(Precision12)/CircleArea(Precision12).cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class CircleArea
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+        double s = Math.PI * r * r;
+        Console.WriteLine("{0:f12}", s);
+    }
+}
+
