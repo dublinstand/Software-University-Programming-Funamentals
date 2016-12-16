cb6a4f6a0b9f226f3c1620496f68813a09f4ecdc
Deyan <dublinstand@gmail.com>
2016-12-16 13:04:27 +0000

    modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln 	new file:   2.Data-Types-and-Methods/26_NumbersInReversedOrder/26_NumbersInReversedOrder.csproj 	new file:   2.Data-Types-and-Methods/26_NumbersInReversedOrder/App.config 	new file:   2.Data-Types-and-Methods/26_NumbersInReversedOrder/NumbersInReversedOrder.cs 	new file:   2.Data-Types-and-Methods/26_NumbersInReversedOrder/Properties/AssemblyInfo.cs 	new file:   2.Data-Types-and-Methods/27_FibonacciNumbers/27_FibonacciNumbers.csproj 	new file:   2.Data-Types-and-Methods/27_FibonacciNumbers/App.config 	new file:   2.Data-Types-and-Methods/27_FibonacciNumbers/FibonacciNumbers.cs 	new file:   2.Data-Types-and-Methods/27_FibonacciNumbers/Properties/AssemblyInfo.cs

---
+++ 2.Data-Types-and-Methods/26_NumbersInReversedOrder/NumbersInReversedOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Write a method that prints the digits of a given decimal number in a reversed order.
+
+class NumbersInReversedOrder
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        reverseNum(number);        
+    }
+
+    static void reverseNum (string number)
+    {
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            Console.Write(number[i]);
+        }
+    }
+}
+
---
+++ 2.Data-Types-and-Methods/27_FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Define a method Fib(n) that calculates the nth Fibonacci number.Examples:
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        fib(n);
+    }
+
+    private static void fib(int n)
+    {
+
+    }
+}
+
