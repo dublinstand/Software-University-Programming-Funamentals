89e5d254bd601cb21056fc62288ea53b317ab615
Deyan <dublinstand@gmail.com>
2016-12-16 11:33:12 +0000

    modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln 	new file:   2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/25_EnglishNameOfTheLastDigit.csproj 	new file:   2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/App.config 	new file:   2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/EnglishNameOfTheLastDigit.cs 	new file:   2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/Properties/AssemblyInfo.cs

---
+++ 2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/EnglishNameOfTheLastDigit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+//Write a method that returns the English name of the last digit of a given number.
+//Write a program that reads an integer and prints the returned value from this method.
+
+class EnglishNameOfTheLastDigit
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        long lastDigit = Math.Abs(n % 10);
+        convertNumber(lastDigit);
+    }
+
+    static void convertNumber(long n)
+    {
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            case 4:
+                Console.WriteLine("four");
+                break;
+            case 5:
+                Console.WriteLine("five");
+                break;
+            case 6:
+                Console.WriteLine("six");
+                break;
+            case 7:
+                Console.WriteLine("seven");
+                break;
+            case 8:
+                Console.WriteLine("eight");
+                break;
+            case 9:
+                Console.WriteLine("nine");
+                break;
+            case 0:
+                Console.WriteLine("zero");
+                break;
+        }
+    }
+}
+
---
+++ 2.Data-Types-and-Methods/25_EnglishNameOfTheLastDigit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("25_EnglishNameOfTheLastDigit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("25_EnglishNameOfTheLastDigit")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20fc963b-2d5b-43f7-a3ed-ad379f6c4fc6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
