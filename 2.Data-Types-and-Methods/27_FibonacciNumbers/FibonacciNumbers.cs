34fa0ca01d2244b289cf8ea9eb7906d3a0622fab
Deyan <dublinstand@gmail.com>
2016-12-23 14:31:20 +0000

    modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln 	modified:   2.Data-Types-and-Methods/27_FibonacciNumbers/27_FibonacciNumbers.csproj 	modified:   2.Data-Types-and-Methods/27_FibonacciNumbers/FibonacciNumbers.cs 	new file:   2.Data-Types-and-Methods/28_PrimeChecker/28_PrimeChecker.csproj 	new file:   2.Data-Types-and-Methods/28_PrimeChecker/App.config 	new file:   2.Data-Types-and-Methods/28_PrimeChecker/PrimeChecker.cs 	new file:   2.Data-Types-and-Methods/28_PrimeChecker/Properties/AssemblyInfo.cs

--- 2.Data-Types-and-Methods/27_FibonacciNumbers/FibonacciNumbers.cs
+++ 2.Data-Types-and-Methods/27_FibonacciNumbers/FibonacciNumbers.cs
@@ -12,7 +12,25 @@ class FibonacciNumbers
 
     private static void fib(int n)
     {
+        int a = 1;
+        int b = 1;
+        int sum = 0;
+        if (n == 1 || n == 0)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                sum = a + b;
+                b = a;
+                a = sum;
+            }
 
+            Console.WriteLine(sum);
+        }
+        
     }
 }
 
---
+++ 2.Data-Types-and-Methods/28_PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class PrimeChecker
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        Console.WriteLine(isPrime(n));
+    }
+
+    private static bool isPrime(long n)
+    {
+        bool isPrime = true;
+
+        if (n <= 1)
+        {
+            isPrime = false;
+            return isPrime;
+        }
+        
+        for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }            
+        }
+
+        return isPrime;
+    }
+}
+
