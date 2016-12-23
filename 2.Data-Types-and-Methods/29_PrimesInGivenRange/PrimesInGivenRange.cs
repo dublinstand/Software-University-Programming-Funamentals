709594b1f2c5dfb81b55535770d4f8b4a63d30f6
Deyan <dublinstand@gmail.com>
2016-12-23 15:49:10 +0000

    modified:   2.Data-Types-and-Methods/2.Data-Types-and-Methods.sln 	modified:   2.Data-Types-and-Methods/28_PrimeChecker/28_PrimeChecker.csproj 	new file:   2.Data-Types-and-Methods/29_PrimesInGivenRange/29_PrimesInGivenRange.csproj 	new file:   2.Data-Types-and-Methods/29_PrimesInGivenRange/App.config 	new file:   2.Data-Types-and-Methods/29_PrimesInGivenRange/PrimesInGivenRange.cs 	new file:   2.Data-Types-and-Methods/29_PrimesInGivenRange/Properties/AssemblyInfo.cs

---
+++ 2.Data-Types-and-Methods/29_PrimesInGivenRange/PrimesInGivenRange.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PrimesInGivenRange
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        List<int> primeNumbers = FindPrimesInRange(a, b);
+
+        string numbers = String.Join(", ", primeNumbers.ToArray());
+        Console.WriteLine(numbers);
+
+        //foreach(int i in primeNumbers)
+        //{
+        //    Console.Write($"{i}, ");          
+
+        //}
+
+    }
+    private static bool isPrime(long n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<int> FindPrimesInRange(int startNum, int endNum)
+    {
+        List<int> numbers = new List<int>();
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            
+            if (isPrime(i))
+            {
+                numbers.Add(i);
+            }
+        }
+
+        return numbers;
+    }
+
+
+}
+
+
+
+
