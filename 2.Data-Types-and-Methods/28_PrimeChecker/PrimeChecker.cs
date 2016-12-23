cf3c623624c156ab37f90f8f35c9f1303b5eea1b
Deyan <dublinstand@gmail.com>
2016-12-23 14:39:59 +0000

    modified:   2.Data-Types-and-Methods/28_PrimeChecker/PrimeChecker.cs

--- 2.Data-Types-and-Methods/28_PrimeChecker/PrimeChecker.cs
+++ 2.Data-Types-and-Methods/28_PrimeChecker/PrimeChecker.cs
@@ -10,24 +10,20 @@ class PrimeChecker
 
     private static bool isPrime(long n)
     {
-        bool isPrime = true;
-
         if (n <= 1)
         {
-            isPrime = false;
-            return isPrime;
+            return false;
         }
         
         for (int i = 2; i <= (int)Math.Sqrt(n); i++)
         {
             if (n % i == 0)
             {
-                isPrime = false;
-                break;
+                return false;
             }            
         }
 
-        return isPrime;
+        return true;
     }
 }
 
