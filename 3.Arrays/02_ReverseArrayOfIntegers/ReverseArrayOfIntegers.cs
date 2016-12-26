02793d5b4237f59cfb677b717377f7b4bd4e9975
Deyan <dublinstand@gmail.com>
2016-12-26 19:19:47 +0000

    new file:   3.Arrays/02_ReverseArrayOfIntegers/02_ReverseArrayOfIntegers.csproj 	new file:   3.Arrays/02_ReverseArrayOfIntegers/App.config 	new file:   3.Arrays/02_ReverseArrayOfIntegers/Properties/AssemblyInfo.cs 	new file:   3.Arrays/02_ReverseArrayOfIntegers/ReverseArrayOfIntegers.cs 	modified:   3.Arrays/3.Arrays.sln

---
+++ 3.Arrays/02_ReverseArrayOfIntegers/ReverseArrayOfIntegers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ReverseArrayOfIntegers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = numbers.Length - 1 ; i >= 0; i--)
+        {
+            Console.Write(numbers[i] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
