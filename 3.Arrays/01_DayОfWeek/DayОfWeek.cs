dd2db9e372892e19d476bb2b19c8592b19afec33
Deyan <dublinstand@gmail.com>
2016-12-26 18:31:10 +0000

    new file:   "3.Arrays/01_Day\320\236fWeek/01_Day\320\236fWeek.csproj" 	new file:   "3.Arrays/01_Day\320\236fWeek/App.config" 	new file:   "3.Arrays/01_Day\320\236fWeek/Day\320\236fWeek.cs" 	new file:   "3.Arrays/01_Day\320\236fWeek/Properties/AssemblyInfo.cs" 	new file:   3.Arrays/3.Arrays.sln

---
+++ 3.Arrays/01_DayОfWeek/DayОfWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+class DayОfWeek
+{
+    static void Main()
+    {
+        string[] week = { "Monday" , "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        if (dayOfWeek >= 1 && dayOfWeek <= 7)
+        {
+            Console.WriteLine(week[dayOfWeek - 1]);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Day!"); 
+        }
+
+    }
+}
+
