4c0c2cd65d5db040ff3a99c36703281635b522af
Deyan <dublinstand@gmail.com>
2016-12-29 10:03:56 +0000

    modified:   3.Arrays/06_RoundingNumbersAwayFromZero/RoundingNumbersAwayFromZero.cs 	new file:   "3.Arrays/07_ReverseArray\320\236fStrings/07_ReverseArray\320\236fStrings.csproj" 	new file:   "3.Arrays/07_ReverseArray\320\236fStrings/App.config" 	new file:   "3.Arrays/07_ReverseArray\320\236fStrings/Properties/AssemblyInfo.cs" 	new file:   "3.Arrays/07_ReverseArray\320\236fStrings/ReverseArray\320\236fStrings.cs" 	modified:   3.Arrays/3.Arrays.sln

--- 3.Arrays/06_RoundingNumbersAwayFromZero/RoundingNumbersAwayFromZero.cs
+++ 3.Arrays/06_RoundingNumbersAwayFromZero/RoundingNumbersAwayFromZero.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Linq;
 
+//Write a program to read an array of real numbers(space separated values), round them to the nearest integer in “away from 0” style and print the output as in the examples below.
+//Rounding in “away from zero” style means:
+//•	To round to the nearest integer, e.g. 2.9  3; -1.75  -2
+
+
 class RoundingNumbersAwayFromZero
 {
     static void Main()
     {
         double[] arr = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"{arr[i]} => {Math.Round(arr[i], MidpointRounding.AwayFromZero)}");
+        }
+
     }
 }
 
---
+++ 3.Arrays/07_ReverseArrayОfStrings/ReverseArrayОfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+//Write a program to read an array of strings, reverse it and print its elements.
+//The input consists of a sequence of space separated strings.
+//Print the output on a single line (space separated).
+
+class ReverseArrayОfStrings
+{
+    static void Main()
+    {
+        string[] arr = Console.ReadLine().Split(' ');
+        string[] reversedArray = arr.Reverse().ToArray();
+
+        Console.WriteLine(string.Join(" ", reversedArray));
+    }
+}
+
