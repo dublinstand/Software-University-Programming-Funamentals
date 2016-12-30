addbe6b0f319614c70999de6dea7b9da4ef9c812
Deyan <dublinstand@gmail.com>
2016-12-30 09:52:05 +0000

    new file:   3.Arrays/11_LargestCommonEnd/11_LargestCommonEnd.csproj 	new file:   3.Arrays/11_LargestCommonEnd/App.config 	new file:   3.Arrays/11_LargestCommonEnd/LargestCommonEnd.cs 	new file:   3.Arrays/11_LargestCommonEnd/Properties/AssemblyInfo.cs 	modified:   3.Arrays/3.Arrays.sln

---
+++ 3.Arrays/11_LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+//Read two arrays of words and find the length of the largest common end(left or right).
+
+class LargestCommonEnd
+{
+    static void Main()
+    {
+        string[] arr1 = Console.ReadLine().Split(' ').ToArray();
+        string[] arr2 = Console.ReadLine().Split(' ').ToArray();
+
+        int minLength = Math.Min(arr1.Length, arr2.Length);
+
+        int count = 0;
+
+
+        if (arr1[0] == arr2[0])
+        {
+            for (int i = 0; i < minLength; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    count++;
+                }
+            }
+        }
+        else if (arr1.Last() == arr2.Last())
+        {
+            string[] arr1Reversed = arr1.Reverse().ToArray();
+            string[] arr2Reversed = arr2.Reverse().ToArray();
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (arr1Reversed[i] == arr2Reversed[i])
+                {
+                    count++;
+                }
+            }
+        }       
+
+        Console.WriteLine(count);
+
+    }
+}
+
