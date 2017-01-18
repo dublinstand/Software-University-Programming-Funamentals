6fc529d63b96ce5ee055e0893b126155823dd940
Deyan <dublinstand@gmail.com>
2017-01-18 17:00:04 +0000

    new file:   6.ObjectAndClasses/01_DayOfWeek/01_DayOfWeek.csproj 	new file:   6.ObjectAndClasses/01_DayOfWeek/App.config 	new file:   6.ObjectAndClasses/01_DayOfWeek/DayOfWeek.cs 	new file:   6.ObjectAndClasses/01_DayOfWeek/Properties/AssemblyInfo.cs 	new file:   6.ObjectAndClasses/6.ObjectAndClasses.sln

---
+++ 6.ObjectAndClasses/01_DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        string dateStr = Console.ReadLine();
+        var tokens = dateStr.Split('-').Select(int.Parse);
+        var date = DateTime.ParseExact(dateStr, "d-M-yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine(date.DayOfWeek);
+    }
+}
