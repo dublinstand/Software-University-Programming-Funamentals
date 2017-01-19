6ab7627ec5f587690d188ba78c7207de74961e73
Deyan <dublinstand@gmail.com>
2017-01-19 16:51:19 +0000

    modified:   6.ObjectAndClasses/01_DayOfWeek/01_DayOfWeek.csproj 	new file:   6.ObjectAndClasses/02_RandomizeWords/02_RandomizeWords.csproj 	new file:   6.ObjectAndClasses/02_RandomizeWords/App.config 	new file:   6.ObjectAndClasses/02_RandomizeWords/Properties/AssemblyInfo.cs 	new file:   6.ObjectAndClasses/02_RandomizeWords/RandomizeWords.cs 	new file:   6.ObjectAndClasses/03_BigFactorial/03_BigFactorial.csproj 	new file:   6.ObjectAndClasses/03_BigFactorial/App.config 	new file:   6.ObjectAndClasses/03_BigFactorial/BigFactorial.cs 	new file:   6.ObjectAndClasses/03_BigFactorial/Properties/AssemblyInfo.cs 	modified:   6.ObjectAndClasses/6.ObjectAndClasses.sln

---
+++ 6.ObjectAndClasses/02_RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class RandomizeWords
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split(' ').ToArray();
+
+        Random rnd = new Random();
+
+        for (int pos1 = 0; pos1 < words.Length; pos1++)
+        {
+
+            var pos2 = rnd.Next(words.Length);
+            var old = words[pos1];
+            words[pos1] = words[pos2];
+            words[pos2] = old;
+        }
+
+        Console.WriteLine(string.Join("\r\n", words));
+
+        //for (int i = 0; i < words.Length; i++)
+        //{
+        //    Console.WriteLine(words[i]);
+        //}
+    }
+}
---
+++ 6.ObjectAndClasses/03_BigFactorial/BigFactorial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class BigFactorial
+{
+    static void Main()
+    {
+
+    }
+}
+
