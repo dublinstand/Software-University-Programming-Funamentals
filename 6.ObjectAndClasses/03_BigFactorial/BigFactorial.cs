9a3435983fb7eeccfe65a48d8fe1e0e3a5e07730
Deyan <dublinstand@gmail.com>
2017-01-19 17:48:03 +0000

    modified:   6.ObjectAndClasses/03_BigFactorial/03_BigFactorial.csproj 	modified:   6.ObjectAndClasses/03_BigFactorial/BigFactorial.cs 	new file:   6.ObjectAndClasses/04_DistanceBetweenPoints/04_DistanceBetweenPoints.csproj 	new file:   6.ObjectAndClasses/04_DistanceBetweenPoints/App.config 	new file:   6.ObjectAndClasses/04_DistanceBetweenPoints/DistanceBetweenPoints.cs 	new file:   6.ObjectAndClasses/04_DistanceBetweenPoints/Properties/AssemblyInfo.cs 	modified:   6.ObjectAndClasses/6.ObjectAndClasses.sln

--- 6.ObjectAndClasses/03_BigFactorial/BigFactorial.cs
+++ 6.ObjectAndClasses/03_BigFactorial/BigFactorial.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 class BigFactorial
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        BigInteger factorial = 1;
+
+        for (int i = 0; i <= n; i++)
+        {
+            factorial *= n;
+        }
+
+        Console.WriteLine(factorial);
 
     }
 }
---
+++ 6.ObjectAndClasses/04_DistanceBetweenPoints/DistanceBetweenPoints.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+class DistanceBetweenPoints
+{
+    static void Main()
+    {
+        Point p1 = ReadPoint();
+        Point p2 = ReadPoint();
+
+        Console.WriteLine("{0:f3}", CalcDistance(p1, p2));
+    }
+
+
+
+    static Point ReadPoint()
+    {
+        int[] tokens = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Point point = new Point();
+        point.X = tokens[0];
+        point.Y = tokens[1];
+
+        return point;
+
+    }
+
+    static double CalcDistance(Point p1, Point p2)
+    {
+        int sideA = Math.Abs(p1.X - p2.X);
+        int sideB = Math.Abs(p1.Y - p2.Y);
+        double shortestDistance = Math.Sqrt(sideA * sideA + sideB * sideB);
+
+        return shortestDistance;
+    }
+}
+
+class Point {
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+
