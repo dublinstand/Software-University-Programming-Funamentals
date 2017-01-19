4cafb13d80652b55de75618858867dc1295f978c
Deyan <dublinstand@gmail.com>
2017-01-19 17:51:08 +0000

    modified:   6.ObjectAndClasses/04_DistanceBetweenPoints/DistanceBetweenPoints.cs

--- 6.ObjectAndClasses/04_DistanceBetweenPoints/DistanceBetweenPoints.cs
+++ 6.ObjectAndClasses/04_DistanceBetweenPoints/DistanceBetweenPoints.cs
@@ -5,15 +5,18 @@ class DistanceBetweenPoints
 {
     static void Main()
     {
-        Point p1 = ReadPoint();
-        Point p2 = ReadPoint();
+        Point p1 = Point.ReadPoint();
+        Point p2 = Point.ReadPoint();
 
-        Console.WriteLine("{0:f3}", CalcDistance(p1, p2));
-    }
+        Console.WriteLine("{0:f3}", Point.CalcDistance(p1, p2));
+    }   
+}
 
+class Point {
+    public int X { get; set; }
+    public int Y { get; set; }
 
-
-    static Point ReadPoint()
+    public static Point ReadPoint()
     {
         int[] tokens = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
@@ -22,10 +25,9 @@ class DistanceBetweenPoints
         point.Y = tokens[1];
 
         return point;
-
     }
 
-    static double CalcDistance(Point p1, Point p2)
+    public static double CalcDistance(Point p1, Point p2)
     {
         int sideA = Math.Abs(p1.X - p2.X);
         int sideB = Math.Abs(p1.Y - p2.Y);
@@ -33,11 +35,6 @@ class DistanceBetweenPoints
 
         return shortestDistance;
     }
-}
-
-class Point {
-    public int X { get; set; }
-    public int Y { get; set; }
 }
 
 
