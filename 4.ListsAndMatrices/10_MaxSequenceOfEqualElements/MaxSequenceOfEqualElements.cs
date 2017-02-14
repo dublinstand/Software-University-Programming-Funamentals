f74b343f89d522451eb82db985980ceff8224720
Deyan <dublinstand@gmail.com>
2017-02-14 17:44:47 +0000

    new file:   4.ListsAndMatrices/10_MaxSequenceOfEqualElements/10_MaxSequenceOfEqualElements.csproj 	new file:   4.ListsAndMatrices/10_MaxSequenceOfEqualElements/App.config 	new file:   4.ListsAndMatrices/10_MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs 	new file:   4.ListsAndMatrices/10_MaxSequenceOfEqualElements/Properties/AssemblyInfo.cs 	modified:   4.ListsAndMatrices/4.ListsAndMatrices.sln

---
+++ 4.ListsAndMatrices/10_MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MaxSequenceOfEqualElements
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        int num = 0;
+        int bestLength = 1;
+        int currentLength = 1;
+        int pos = 0;
+        
+
+        while (pos != nums.Count - 1)
+        {
+            if (nums[pos] == nums[pos + 1])
+            {
+                currentLength++;
+
+                if (bestLength < currentLength)
+                {
+                    num = nums[pos];
+                    bestLength = currentLength;
+                    
+                }
+            }
+            else
+            {                
+                currentLength = 1;            
+            }          
+
+            pos++;
+
+            if (bestLength == 1)
+            {
+                num = nums[0];
+            }
+        }
+
+        for (int i = 0; i <= bestLength; i++)
+        {
+            Console.Write($"{num} ");
+        }
+
+    }
+}
+
