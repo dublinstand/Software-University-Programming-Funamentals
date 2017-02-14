099c3df8da9dc302e063743eda32efa10e612b96
Deyan <dublinstand@gmail.com>
2017-02-14 13:30:16 +0000

    modified:   4.ListsAndMatrices/08_BuildMatrixOfLetters/08_BuildMatrixOfLetters.csproj 	modified:   4.ListsAndMatrices/08_BuildMatrixOfLetters/BuildMatrixOfLetters.cs 	new file:   4.ListsAndMatrices/09_RotateMatrix/09_RotateMatrix.csproj 	new file:   4.ListsAndMatrices/09_RotateMatrix/App.config 	new file:   4.ListsAndMatrices/09_RotateMatrix/Properties/AssemblyInfo.cs 	new file:   4.ListsAndMatrices/09_RotateMatrix/RotateMatrix.cs 	modified:   4.ListsAndMatrices/4.ListsAndMatrices.sln

--- 4.ListsAndMatrices/08_BuildMatrixOfLetters/BuildMatrixOfLetters.cs
+++ 4.ListsAndMatrices/08_BuildMatrixOfLetters/BuildMatrixOfLetters.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 
+//Build and print a matrix of capital Latin letters of size rows x cols like at the example below.
+
 class BuildMatrixOfLetters
 {
     static void Main()
     {
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
 
+        //first we set up the rows and in the for loop we set up for each row number of cols
+        char[][] matrix = new char[rows][];
+        char letter = 'A';
+
+        for (int row = 0; row < rows; row++)
+        {
+            //first we set up the rows and in the for loop we set up for each row number of cols
+            matrix[row] = new char[cols];
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row][col] = letter;
+                letter++;
+            }            
+        }
+
+        for (int row = 0; row < rows; row++)
+        {
+            Console.WriteLine(string.Join(" ", matrix[row]));
+        }
     }
 }
 
