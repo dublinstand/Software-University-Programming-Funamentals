e1ead00fa63a7958071abec1ad9a82ca26ef1762
Deyan <dublinstand@gmail.com>
2017-02-20 13:53:38 +0000

    new file:   4.ListsAndMatrices/15_SumReversedNumbers/15_SumReversedNumbers.csproj 	new file:   4.ListsAndMatrices/15_SumReversedNumbers/App.config 	new file:   4.ListsAndMatrices/15_SumReversedNumbers/Properties/AssemblyInfo.cs 	new file:   4.ListsAndMatrices/15_SumReversedNumbers/SumReversedNumbers.cs 	new file:   4.ListsAndMatrices/16_BombNumbers/16_BombNumbers.csproj 	new file:   4.ListsAndMatrices/16_BombNumbers/App.config 	new file:   4.ListsAndMatrices/16_BombNumbers/BombNumbers.cs 	new file:   4.ListsAndMatrices/16_BombNumbers/Properties/AssemblyInfo.cs 	modified:   4.ListsAndMatrices/4.ListsAndMatrices.sln

---
+++ 4.ListsAndMatrices/15_SumReversedNumbers/SumReversedNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Write a program that reads sequence of numbers, reverses their digits, and prints their sum.
+
+
+class SumReversedNumbers
+{
+    static void Main()
+    {
+        string[] nums = Console.ReadLine().Split();
+
+        List<int> revNums = reverseAllNums(nums);
+        //Console.WriteLine(string.Join(", ", revNums));
+
+        int sum = sumNums(revNums);
+        Console.WriteLine(sum);
+
+    }
+
+    private static int sumNums(List<int> revNums)
+    {
+        int sum = 0;
+
+        foreach (int num in revNums)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    private static List<int> reverseAllNums(string[] nums)
+    {
+        List<int> newNums = new List<int>();
+
+        foreach (string num in nums)
+        {
+            string temp = "";
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                temp += num[i];
+            }
+
+            newNums.Add(int.Parse(temp));
+        }
+        return newNums;
+    }
+}
+
---
+++ 4.ListsAndMatrices/16_BombNumbers/BombNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Write a program that reads sequence of numbers and special bomb number with a certain power.
+//    Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. 
+//    Detonations are performed from left to right and all detonated numbers disappear. 
+//    Finally print the sum of the remaining elements in the sequence.
+
+class BombNumbers
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int[] token = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int bombNumber = token[0];
+        int bombPower = token[1];
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (nums[i] == bombNumber)
+            {
+                int left = Math.Max(i - bombPower, 0);
+
+                int right = Math.Min(i + bombPower, nums.Count - 1);
+
+                int lenght = right - left + 1;
+                nums.RemoveRange(left, lenght);
+                i = 0;
+            }
+        }
+        Console.WriteLine(nums.Sum());
+
+
+    }
+
+}
+
